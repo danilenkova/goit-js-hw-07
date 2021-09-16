444417085be0805a7a64c018972b26dcce58e9d4
Yuliia Danilenkova <82413695+danilenkova@users.noreply.github.com>
2021-09-16 19:15:42 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,18 +1,11 @@
 const inputEL = document.querySelector('#validation-input');
-const validLength = inputEL.getAttribute('data-length');
 
 inputEL.addEventListener('blur', onInputBlur);
 
 function onInputBlur(event) {
-    if (inputEL.value.length == validLength) {
-        if (inputEL.classList.contains('invalid')) {
-            inputEL.classList.replace('invalid', 'valid')
-        } else {
-            inputEL.classList.add('valid');
-        }
-    } else if (inputEL.classList.contains('valid')) {
-        inputEL.classList.replace('valid', 'invalid')
+    if (inputEL.value.length == inputEL.getAttribute('data-length')) {
+        inputEL.className = 'valid';
     } else {
-        inputEL.classList.add('invalid');
+        inputEL.className = 'invalid';
     }
 }
